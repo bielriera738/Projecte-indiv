fae0ee681fe243e4efd292428938a6bdb4535c61
bielriera738 <biriba@prateducacio.com>
2025-10-30 16:11:15 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/menu.dart';
+
+void main() {
+  runApp(const NutriVisionApp());
+}
+
+class NutriVisionApp extends StatelessWidget {
+  const NutriVisionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NutriVision AI',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF0B3D2E),
+        scaffoldBackgroundColor: const Color(0xFF0B3D2E),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: const MenuScreen(),
+    );
+  }
+}
